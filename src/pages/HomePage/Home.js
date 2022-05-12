e94749c910cbbecd144316865c28eda62c0ffef3
witerowson <runurowson@gmail.com>
2022-05-12 09:06:29 +0600

    treatment

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -3,14 +3,21 @@ import Banner from './Banner';
 
 import Info from './Info/Info';
 import Services from './Services';
-
+import bgImage from '../../images/bg.png';
+import AnotherService from './AnotherService';
 const Home = () => {
     return (
         <div className='px-12'>
+            <div style={{
+                backgroundImage: `url(${bgImage})`,
+                backgroundSize: "cover",
+            }}>
 
-            <Banner></Banner>
+                <Banner></Banner>
+            </div>
             <Info></Info>
             <Services></Services>
+            <AnotherService></AnotherService>
 
         </div >
     );
